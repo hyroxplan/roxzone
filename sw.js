819a8ba4c7725b86f7017e49eadfa9a03aef76e4
matthieuclanet-droid <matthieuclanet@gmail.com>
2026-09-13 20:39:57 +0200

    v1.4 : module sommeil (conseils du soir, journal de nuit, adaptation)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Roxzone — service worker : application utilisable hors ligne.
    Incrémenter VERSION à chaque déploiement pour invalider le cache. */
-var VERSION = "roxzone-2026-09-13d";
+var VERSION = "roxzone-2026-09-13e";
 var SHELL = [
   "./",
   "./index.html",
